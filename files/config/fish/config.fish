20b8215d6f7d296575461d6a472f84b8a24f7fb1
Ryan Patterson <cgamesplay@cgamesplay.com>
2014-03-12 22:52:33 -0700

    Add oh-my-fish

---
+++ files/config/fish/config.fish
@@ -0,0 +1,13 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.config/oh-my-fish
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+set fish_plugins rvm bundler
+
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
