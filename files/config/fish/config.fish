9d5b0b0274a218948108059b072a02c0e4254527
Ryan Patterson <ryan@simplyinsured.com>
2016-08-17 13:21:29 -0700

    Changes to git config etc

--- files/config/fish/config.fish
+++ files/config/fish/config.fish
@@ -4,6 +4,6 @@ set -x LESS "-RS"
 source ~/.config/fish/iterm2_shell_integration.fish
 
 # Load hterminal shell integration
-if [ ! -z $HTERMINAL_ROOT ]
+if [ ! -z "$HTERMINAL_ROOT" ]
   source $HTERMINAL_ROOT/share/shell/fish/config.fish
 end
