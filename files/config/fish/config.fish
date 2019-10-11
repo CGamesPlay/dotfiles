1a6b068728c0dbcc614316f1748a86b9097f510a
Ryan Patterson <cgamesplay@cgamesplay.com>
2019-10-11 18:42:16 +0400

    Config changes from mac

--- files/config/fish/config.fish
+++ files/config/fish/config.fish
@@ -2,6 +2,12 @@ set -x EDITOR vim
 set -x LESS "-RSF"
 set -x PATH ./node_modules/.bin $PATH
 
+if [ ! -z "$GOPATH" ]
+  set -x PATH $GOPATH/bin $PATH
+  set -x VIRTUALGO_DISABLE_PROMPT 1
+  $GOPATH/bin/vg eval --shell fish | source
+end
+
 eval (python -m virtualfish)
 
 source ~/.config/fish/iterm2_shell_integration.fish
