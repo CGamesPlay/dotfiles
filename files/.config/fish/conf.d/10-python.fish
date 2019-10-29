9d79ac3abac19ad8b83848100a660eda8057caef
Ryan Patterson <cgamesplay@cgamesplay.com>
2019-10-29 18:17:22 +0400

    virtualfish improvements

--- files/.config/fish/conf.d/10-python.fish
+++ files/.config/fish/conf.d/10-python.fish
@@ -1,3 +1,5 @@
 # Set up virtualenv, if installed.
 
-python -m virtualfish 2>/dev/null | source
+if begin; command -qs python; and python -c 'import pkgutil; exit(0 if pkgutil.find_loader("virtualfish") else 1)'; end
+  python -m virtualfish auto_activation 2>/dev/null | source
+end
