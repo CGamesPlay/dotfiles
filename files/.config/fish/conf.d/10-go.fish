9483928daf948ed08177c5488f2927f828fc7735
Ryan Patterson <cgamesplay@cgamesplay.com>
2019-10-29 18:07:19 +0400

    change hash to fish builtin

--- files/.config/fish/conf.d/10-go.fish
+++ files/.config/fish/conf.d/10-go.fish
@@ -1,6 +1,6 @@
 # Set up GOPATH and optionally virtualgo, if go is installed.
 
-if ! hash go 2>/dev/null
+if ! command -qs go 2>/dev/null
   exit
 end
 
@@ -8,7 +8,7 @@ mkdir -p ~/.go
 set -x GOPATH ~/.go
 set -x PATH ~/.go/bin $PATH
 
-if hash vg 2>/dev/null
+if command -qs vg 2>/dev/null
   set -x VIRTUALGO_DISABLE_PROMPT 1
   vg eval --shell fish | source
 end
