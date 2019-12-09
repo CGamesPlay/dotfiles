fcc9f9ba502bee97a6af97937b00166755b62def
Ryan Patterson <cgamesplay@cgamesplay.com>
2019-12-09 17:09:19 +0400

    Remove all the individual per-directory environment hooks; prefer direnv

--- files/.config/fish/conf.d/10-python.fish
+++
@@ -1,5 +0,0 @@
-# Set up virtualenv, if installed.
-
-if begin; command -qs python; and python -c 'import pkgutil; exit(0 if pkgutil.find_loader("virtualfish") else 1)'; end
-  python -m virtualfish auto_activation 2>/dev/null | source
-end
--- files/.config/fish/conf.d/20-fnm.fish
+++
@@ -1,8 +0,0 @@
-if [ ! -d ~/.fnm ]
-  exit
-end
-
-set PATH ~/.fnm $PATH
-fnm env --multi --shell=fish | source
-# FNM can't be set to use the system by default, brilliant.
-fnm use system >/dev/null
