09bbd36edcdd35fd09a6e97caf3c7dd93053d998
Ryan Patterson <cgamesplay@cgamesplay.com>
2019-12-09 13:40:17 +0400

    misc fish changes

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -1,3 +1,7 @@
 set -x EDITOR vim
 set -x LESS "-RSF"
 set -x PATH ./node_modules/.bin $PATH
+
+# Clear fzf's ^T binding to leave transpose alone, and use ^P instead
+bind \ct
+bind \cp fzf-file-widget
