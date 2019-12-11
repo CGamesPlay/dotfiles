af3bed4aa5d3c6119024d1f9323fb4dc2af82233
Ryan Patterson <cgamesplay@cgamesplay.com>
2019-12-11 14:20:50 +0400

    Open command for linux, ls deterrent

---
+++ files/.config/fish/functions/ls.fish
@@ -0,0 +1,6 @@
+# Defined in /tmp/fish.keiMl2/ls.fish @ line 2
+function ls
+	echo "Remember to save keystrokes with `l`!"
+sleep 1
+command ls -G $argv
+end
---
+++ linux/.config/fish/functions/open.fish
@@ -0,0 +1,4 @@
+# Defined in /tmp/fish.Jhw6yr/open.fish @ line 1
+function open
+	xdg-open $argv
+end
