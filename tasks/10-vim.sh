68f5f89c8b988e2ab8c134a9bd44ba04ebf52f08
Ryan Patterson <cgamesplay@cgamesplay.com>
2020-01-29 13:52:42 +0400

    Move vim configs to XDG-compliant locations

--- tasks/10-vim.sh
+++ tasks/10-vim.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-mkdir -p ~/.vim/bundle ~/.vim/swaps
-cd ~/.vim/bundle
+mkdir -p ~/.config/vim/bundle
+cd ~/.config/vim/bundle
 [ ! -d Vundle.vim ] && git clone https://github.com/gmarik/Vundle.vim.git
 vim -c VundleInstall -c qa
