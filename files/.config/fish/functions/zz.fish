4a6e4a83f251107d253f0225e605fa670ac1c2ed
Ryan Patterson <cgamesplay@cgamesplay.com>
2021-03-31 00:31:52 +0800

    latest changes, include zz

--- files/.config/fish/functions/zz.fish
+++ files/.config/fish/functions/zz.fish
@@ -1,8 +1,9 @@
-function zz -d "Jump to a directory with fasd"
+# Defined in /var/folders/2r/0y4kz7r53y1bflwy7l87z4n40000gn/T//fish.EjGOwt/zz.fish @ line 2
+function zz --description 'Jump to a directory with fasd'
   fasd -Rdl | \
     FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --height 40% --reverse" \
     fzf --query=$argv | \
     read -l ret
   test -z "$ret"; and return
-  cd "$ret"
+  cd "$ret" && pwd
 end
