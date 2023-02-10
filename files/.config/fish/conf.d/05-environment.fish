f9c08cb136b738a085b1f7c58c348650a3dc1c40
Ryan Patterson <cgamesplay@cgamesplay.com>
2023-02-10 10:44:56 +0400

    Small XDG cleanup

--- files/.config/fish/conf.d/05-environment.fish
+++ files/.config/fish/conf.d/05-environment.fish
@@ -9,6 +9,18 @@ if ! set -q XDG_CACHE_HOME
     set -x XDG_CACHE_HOME ~/.cache
   end
 end
+if ! set -q XDG_DATA_HOME
+  set -x XDG_DATA_HOME ~/.local/share
+end
+if ! set -q XDG_CONFIG_HOME
+  set -x XDG_CONFIG_HOME ~/.config
+end
+if ! set -q XDG_STATE_HOME
+  set -x XDG_STATE_HOME ~/.local/state
+end
+if ! set -q XDG_RUNTIME_DIR
+  set -x XDG_RUNTIME_DIR $TMPDIR
+end
 
 # When inside of an SSH connection, establish a well-known location for
 # SSH_AUTH_SOCK, so that sessions in tmux can keep using the same socket address
