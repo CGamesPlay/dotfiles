6b4c8e604f2afa590dee6194bef1304c88ebf2f2
Ryan Patterson <cgamesplay@cgamesplay.com>
2023-05-29 08:49:13 +0400

    Install argc

---
+++ tasks/50-argc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Install robo to ~/.local/bin
+set -e
+. "$(dirname "$0")/helpers"
+
+TARGET=~/.local/bin/argc
+VERSION=1.2.0
+
+if [ -x $TARGET ]; then
+  exit 0
+fi
+
+url=https://github.com/sigoden/argc/releases/download/v$VERSION/argc-v${VERSION}-$(rust_triple).tar.gz
+curl -fsSL $url -o argc.tar.gz
+tar -xf argc.tar.gz --directory=$(dirname $TARGET) argc
+chmod +x ${TARGET}
+rm argc.tar.gz
+echo "Installed argc $($TARGET --argc-version) at $TARGET"
