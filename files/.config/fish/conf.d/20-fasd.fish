8a396216fb30040f121c156fd2d96f86668867c9
Ryan Patterson <cgamesplay@cgamesplay.com>
2023-06-21 12:21:13 +0400

    revert some of fasd changes due to grep -P

--- files/.config/fish/conf.d/20-fasd.fish
+++ files/.config/fish/conf.d/20-fasd.fish
@@ -7,14 +7,6 @@ if ! command -qs fasd 2>/dev/null
 end
 
 function __fasd_expand_vars -d "Expands only the first occurance of a variable in the passed string without evaluating the string"
-  set -lx vars (echo -n $argv | grep -oP '(?!\\\\)\$\K([A-z_][A-z0-9_]*?)([^A-z0-9_]|\b|\n)' | perl -pe 's/(.+?)(?:[^A-z0-9_]|\b)$/\1\n/' | sort -u)
-  for var in $vars
-    # Only replace if the variable is defined
-    if set -q $var
-      # Replacing the variable once is enough
-      set argv (string replace -r '([^\\\\]|\b)\$'"$var" '$1'"$$var" "$argv")
-    end
-  end
   # The following pipe does the same thing as fasd --sanitize
   printf '%s\\n' "$argv" | sed -e 's/\([^\]\)$( *[^ ]* *\([^)]*\)))*/\1\2/g' -e 's/\([^\]\)[|&;<>$`{}]\{1,\}/\1 /g' | tr -s " " \n
 end
