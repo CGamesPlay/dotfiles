20978e0556c31876950b17ff673a7098bb9bd8c7
Ryan Patterson <cgamesplay@cgamesplay.com>
2024-03-01 11:05:09 +0530

    Fix bootstrap installer on macos

--- tasks/05-deps.sh
+++ tasks/05-deps.sh
@@ -3,10 +3,22 @@
 set -e
 . "$(dirname "$0")/helpers"
 
-if is_installed apt 2>/dev/null; then
-    sudo apt-get update
-    sudo apt-get install -y curl tar git vim tmux fish direnv
-else
-    echo "Don't know how to install on this platform" >&2
-    exit 1
+packages=()
+type curl >/dev/null || packages+=(curl)
+type tar >/dev/null || packages+=(tar)
+type git >/dev/null || packages+=(git)
+type vim >/dev/null || packages+=(vim)
+type tmux >/dev/null || packages+=(tmux)
+type fish >/dev/null || packages+=(fish)
+type direnv >/dev/null || packages+=(direnv)
+
+if [[ "${#packages[@]}" -gt 0 ]]; then
+	if is_installed apt-get 2>/dev/null; then
+		sudo apt-get update
+		sudo apt-get install -y curl tar git vim tmux fish direnv
+	else
+		echo "Missing packages: ${packages[*]}" >&2
+		echo "Don't know how to install on this platform" >&2
+		exit 1
+	fi
 fi
