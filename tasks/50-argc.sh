b2057130bcd86cfb68a9dd0a3b78c8f13297d22d
Ryan Patterson <cgamesplay@cgamesplay.com>
2024-03-04 12:32:46 +0530

    Quality of life improvements for @env

--- tasks/50-argc.sh
+++ tasks/50-argc.sh
@@ -4,15 +4,15 @@ set -e
 . "$(dirname "$0")/helpers"
 
 TARGET=~/.local/bin/argc
-VERSION=1.7.0
+VERSION=1.15.0
 
 if [ -x $TARGET ] && $TARGET --argc-version | grep -q $VERSION; then
   exit 0
 fi
 
-url=https://github.com/sigoden/argc/releases/download/v$VERSION/argc-v${VERSION}-$(rust_triple).tar.gz
-curl -fsSL $url -o argc.tar.gz
-tar -xf argc.tar.gz --directory=$(dirname $TARGET) argc
-chmod +x ${TARGET}
+url="https://github.com/sigoden/argc/releases/download/v$VERSION/argc-v${VERSION}-$(rust_triple).tar.gz"
+curl -fsSL "$url" -o argc.tar.gz
+tar -xf argc.tar.gz --directory="$(dirname "$TARGET")" argc
+chmod +x "${TARGET}"
 rm argc.tar.gz
 echo "Installed argc $($TARGET --argc-version) at $TARGET"
