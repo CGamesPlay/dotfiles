493e634db734f53e222f6ea817d86e10b3364f07
Ryan Patterson <cgamesplay@cgamesplay.com>
2024-03-04 13:09:17 +0530

    Fix deps

--- tasks/05-deps.sh
+++ tasks/05-deps.sh
@@ -4,19 +4,19 @@ set -e
 . "$(dirname "$0")/helpers"
 
 packages=()
-type curl >/dev/null || packages+=(curl)
-type tar >/dev/null || packages+=(tar)
-type git >/dev/null || packages+=(git)
-type vim >/dev/null || packages+=(vim)
-type tmux >/dev/null || packages+=(tmux)
-type fish >/dev/null || packages+=(fish)
-type direnv >/dev/null || packages+=(direnv)
-type socat >/dev/null || packages+=(socat)
+type curl >/dev/null 2>&1 || packages+=(curl)
+type tar >/dev/null 2>&1 || packages+=(tar)
+type git >/dev/null 2>&1 || packages+=(git)
+type vim >/dev/null 2>&1 || packages+=(vim)
+type tmux >/dev/null 2>&1 || packages+=(tmux)
+type fish >/dev/null 2>&1 || packages+=(fish)
+type direnv >/dev/null 2>&1 || packages+=(direnv)
+type socat >/dev/null 2>&1 || packages+=(socat)
 
 if [[ "${#packages[@]}" -gt 0 ]]; then
 	if is_installed apt-get 2>/dev/null; then
 		sudo apt-get update
-		sudo apt-get install -y curl tar git vim tmux fish direnv
+		sudo apt-get install -y "${packages[@]}"
 	else
 		echo "Missing packages: ${packages[*]}" >&2
 		echo "Don't know how to install on this platform" >&2
