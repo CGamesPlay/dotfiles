ca006a2373d9761ccd1a6cfaa4e6be1d1fb56e91
Ryan Patterson <cgamesplay@cgamesplay.com>
2024-10-16 11:45:31 +0800

    Improve shutdown script

--- devserver/scripts/install-ansible.sh
+++ devserver/scripts/install-ansible.sh
@@ -6,14 +6,14 @@ cd /opt/ansible
 cat >requirements.txt <<'EOF'
 ansible==6.3.0
 ansible-core==2.13.3
-cffi==1.15.1
+cffi==1.17.1
 cryptography==37.0.4
 Jinja2==3.1.2
 MarkupSafe==2.1.1
 packaging==21.3
 pycparser==2.21
 pyparsing==3.0.9
-PyYAML==6.0
+PyYAML==6.0.2
 resolvelib==0.8.1
 EOF
 .venv/bin/pip3 install -r requirements.txt
