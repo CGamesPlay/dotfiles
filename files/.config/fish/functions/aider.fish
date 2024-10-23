5cdabe9488c48377fa4b936e7ffb5b670c4cb77a
Ryan Patterson <cgamesplay@cgamesplay.com>
2024-10-24 07:13:51 +0800

    Add aider config file

---
+++ files/.config/fish/functions/aider.fish
@@ -0,0 +1,3 @@
+function aider
+  command aider -c ~/.config/aider/conf.yml $argv
+end
--- files/.config/fish/functions/ec2_ssh.fish
+++ files/.config/fish/functions/ec2_ssh.fish
@@ -1,4 +1,3 @@
-# Defined in /var/folders/2r/0y4kz7r53y1bflwy7l87z4n40000gn/T//fish.soO7m6/ec2_ssh.fish @ line 2
-function ec2_ssh
+function ec2_ssh --wraps ssh
   ssh -F ~/.ssh/ec2_config $argv
 end
