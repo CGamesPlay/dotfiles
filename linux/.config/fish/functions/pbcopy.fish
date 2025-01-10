eeaa64bf3f760ba31f452b3437c753935eca9de8
Ryan Patterson <cgamesplay@cgamesplay.com>
2025-01-10 16:08:55 +0800

    Improve clipboard handling in tmux sessions

--- linux/.config/fish/functions/pbcopy.fish
+++ linux/.config/fish/functions/pbcopy.fish
@@ -1,5 +1,9 @@
 # I have muscle memory for using these commands from macos, and the linux
 # replacements are long commands anyways.
 function pbcopy --description "Save input to the system clipboard"
-  xclip -selection clipboard
+  if set -q TMUX
+    tmux loadb -
+  else
+    xclip -selection clipboard
+  end
 end
--- linux/.config/fish/functions/pbpaste.fish
+++ linux/.config/fish/functions/pbpaste.fish
@@ -1,5 +1,9 @@
 # I have muscle memory for using these commands from macos, and the linux
 # replacements are long commands anyways.
 function pbpaste --description "Output contents of the system clipboard"
-  xclip -selection clipboard -o
+  if set -q TMUX
+    tmux saveb -
+  else
+    xclip -selection clipboard -o
+  end
 end
