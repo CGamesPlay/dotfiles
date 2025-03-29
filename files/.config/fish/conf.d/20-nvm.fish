053495c172b7a00d363d8380f21a1d2046136853
Ryan Patterson <cgamesplay@cgamesplay.com>
2025-03-29 09:39:41 +0800

    move NVM_DIR

--- files/.config/fish/conf.d/20-nvm.fish
+++ files/.config/fish/conf.d/20-nvm.fish
@@ -1,5 +1,10 @@
 if [ -d /usr/local/share/nvm ]
     set -x NVM_DIR /usr/local/share/nvm
 else
-    set -x NVM_DIR ~/.config/nvm
+    # Migration
+    if [ -d ~/.config/nvm ]
+        mkdir -p ~/.local/share
+        mv ~/.config/nvm ~/.local/share/nvm
+    end
+    set -x NVM_DIR ~/.local/share/nvm
 end
