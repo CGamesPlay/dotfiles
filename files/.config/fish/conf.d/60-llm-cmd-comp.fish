b3537cdfdee267c40c0461d0f0411932724c511c
Ryan Patterson <cgamesplay@cgamesplay.com>
2025-07-06 14:56:35 +0800

    improvements to llm cmd comp

--- files/.config/fish/conf.d/60-llm-cmd-comp.fish
+++ files/.config/fish/conf.d/60-llm-cmd-comp.fish
@@ -1,10 +1,16 @@
 bind \cu __llm_cmdcomp
 
 function __llm_cmdcomp -d "Fill in the command using an LLM"
+  echo # Start the program on a blank line
+  if ! command -q llm
+    echo "llm is not installed"
+    echo
+    commandline -f repaint
+    return
+  end
   set __llm_oldcmd (commandline -b)
   set __llm_cursor_pos (commandline -C)
-  echo # Start the program on a blank line
-  set result (llm cmdcomp -m gpt-4o $__llm_oldcmd)
+  set result (llm cmdcomp $__llm_oldcmd)
   commandline -f repaint
   if test $status -eq 0
     commandline -r $result
