53849f8783afcf370b373eda8853a8a5404d0fb5
Ryan Patterson <cgamesplay@cgamesplay.com>
2025-10-13 12:35:11 +0800

    jj: a variety of config tweaks

--- files/.config/nvim/lua/config/diff.lua
+++ files/.config/nvim/lua/config/diff.lua
@@ -1,6 +1,12 @@
--- Provides a mergetool.
--- Call :DiffConflicts to convert a file containing conflict markers into a two-way diff.
 return {
-  "whiteinge/diffconflicts",
-  cmd = { "DiffConflicts", "DiffConflictsWithHistory" }
+  -- Provides a mergetool.
+  -- Call :DiffConflicts to convert a file containing conflict markers into a two-way diff.
+  {
+    "whiteinge/diffconflicts",
+    cmd = { "DiffConflicts", "DiffConflictsWithHistory" }
+  },
+  {
+    "rafikdraoui/jj-diffconflicts",
+    cmd = "JJDiffConflicts"
+  }
 }
