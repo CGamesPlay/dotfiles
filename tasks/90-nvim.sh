8e40a913fbef03a23910b46df1c867909df212ef
Ryan Patterson <cgamesplay@cgamesplay.com>
2025-10-17 08:03:02 +0000

    nvim: try installing from appimage instead of ppa

--- tasks/90-nvim.sh
+++ tasks/90-nvim.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Allow Lazy to install all of its plugins.
-set -e
+set -eu
 
 preferred_version="0.11"
 
@@ -17,32 +17,18 @@ install_neovim() {
 			return 0
 		fi
 	fi
-	if [ -f /etc/lsb-release ]; then
-		# shellcheck disable=SC1091
-		. /etc/lsb-release
-		# https://launchpad.net/~neovim-ppa/+archive/ubuntu/unstable
-		mkdir -p /etc/apt/keyrings
-		curl -fsSL 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9DBB0BE9366964F134855E2255F96FCF8231B6DD' | sudo tee /etc/apt/keyrings/ppa-neovim-ppa.asc >/dev/null
-		sudo tee /etc/apt/sources.list.d/ppa-neovim-ppa-unstable.sources >/dev/null <<-EOF
-		Types: deb
-		URIs: https://ppa.launchpadcontent.net/neovim-ppa/unstable/ubuntu
-		Suites: $DISTRIB_CODENAME
-		Components: main
-		Signed-By: /etc/apt/keyrings/ppa-neovim-ppa.asc
 
-		Types: deb-src
-		URIs: https://ppa.launchpadcontent.net/neovim-ppa/unstable/ubuntu
-		Suites: $DISTRIB_CODENAME
-		Components: main
-		Signed-By: /etc/apt/keyrings/ppa-neovim-ppa.asc
-		EOF
-
-		sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/ppa-neovim-ppa-unstable.sources" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
-		sudo DEBIAN_FRONTEND=noninteractive apt-get install -y neovim
+	if [ "$(uname -m)" = "x86_64" ]; then
+		tag="x86_64"
 	else
-		echo "Don't know how to install neovim on this platform" >&2
-		return 1
+		tag="arm64"
 	fi
+	cd "${TMPDIR:-/tmp}"
+	mkdir nvim
+	cd nvim
+	eget neovim/neovim -a "$tag" --to="./nvim"
+	./nvim --appimage-extract
+	sudo rsync -a squashfs-root/usr/ /usr/local/
 }
 
 sync_plugins() {
