3a4edaac51779def8b2d686354e804d35f537e9a
Ryan Patterson <cgamesplay@cgamesplay.com>
2025-10-24 00:14:15 +0800

    minor cleanups

--- files/.config/fish/conf.d/10-path.fish
+++ files/.config/fish/conf.d/10-path.fish
@@ -1,3 +1,9 @@
+if [ -d ~/.cargo/bin ]
+  fish_add_path -mpg ~/.cargo/bin
+end
+if [ -d ~/.local/bin ]
+  fish_add_path -mpg ~/.local/bin
+end
 if [ -d /usr/local/sbin ]
   fish_add_path -g /usr/local/sbin
 end
@@ -6,12 +12,6 @@ if [ -d /usr/local/bin ]
 end
 if [ -d /opt/homebrew/bin ]
   fish_add_path -g /opt/homebrew/bin
-end
-if [ -d ~/.cargo/bin ]
-  fish_add_path -g ~/.cargo/bin
-end
-if [ -d ~/.local/bin ]
-  fish_add_path -g ~/.local/bin
 end
 if [ -d ~/.local/bin-after ]
   fish_add_path -maP ~/.local/bin-after
