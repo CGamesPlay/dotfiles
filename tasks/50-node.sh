7aa9a2ca7c91c385eca9d14ddeb71d318de5e867
Ryan Patterson <cgamesplay@cgamesplay.com>
2025-11-10 23:25:53 +0800

    change how nvm is installed + misc

--- tasks/50-node.sh
+++ tasks/50-node.sh
@@ -9,7 +9,9 @@ if [ -d /usr/local/share/nvm ]; then
 else
 	export NVM_DIR=~/.local/share/nvm
 	if ! [ -d "$NVM_DIR" ]; then
-		@get nvm
+		git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+		cd "$NVM_DIR"
+		git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" "$(git rev-list --tags --max-count=1)")"
 	fi
 fi
 
